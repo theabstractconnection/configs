679368a70b3cc2619aa7b15ad059fe609dbdb240
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-20 22:25:54 +0100

    update ec2-setup.sh for script usage

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -1,87 +1,112 @@
+read -p  "Enter your Localhost username : " LOCAL_USERNAME
+read -sp "Enter your Localhost password : " LOCAL_PASSWORD && echo
+read -p  "Enter your Github full name   : " GITHUB_FULL_NAME
+read -p  "Enter your Github email       : " GITHUB_EMAIL
+read -sp "Enter your Github password    : " GITHUB_PASSWORD && echo
+read -s  "Enter your Github GPG key id  : " GITHUB_GPG_KEY_ID && echo
+
+set -e
+
 # CHANGE USER PASSWORD
-sudo passwd ec2-user
+# sudo passwd ec2-user
+echo "*** Updating $USER password"
+echo $LOCAL_PASSWORD | sudo passwd ec2-user --stdin 
+
 # UPDATE SYSTEM
+echo "*** Updating system"
 sudo yum update -y
 
 # INSTALL ZSH GIT OH-MY-ZSH & CHANGE SHELL to ZSH
-sudo yum install -y util-linux-user zsh git
+echo "*** Insalling util-linux-user zsh git"
+sudo yum install util-linux-user zsh git -y
+echo "*** Changing shell for $USER to zsh"
 chsh -s $(which zsh)
+echo "*** Installing oh-my-zsh"
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+# INSTALL ZSH THEME
+echo "*** Installing ZSH theme"
+export ZSH="/home/$USER/.oh-my-zsh"
+export ZSH_CUSTOM="$ZSH/custom"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k 2>&1
+
+# ADD MISSING PLUGINS
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting 2>&1
+
 # CLONE DOTFILES
-git clone https://github.com/abstracts33d/dotfiles.git
+echo "*** Cloning Dotfiles"
+git clone https://$GITHUB_USERNAME:$GITHUB_PASSWORD@https://github.com/abstracts33d/dotfiles.git 2>&1
+
+# INSTALLING SSHPASS
+echo "*** Installing sshpass"
+wget http://sourceforge.net/projects/sshpass/files/sshpass/1.06/sshpass-1.06.tar.gz/download 
+mv download sshpass-1.06.tar.gz
+gunzip sshpass-1.06.tar.gz
+tar xvf sshpass-1.06.tar
+cd sshpass-1.06
+sudo yum groupinstall "Development Tools"
+sudo ./configure
+sudo make install
 
 # COPY SSH/GPG KEYS & IMPORT GPG KEY
-mkdir .keys
-mkdir .keys/ssh
-mkdir .keys/gpg
-mkdir .keys/ssl
-
-scp -P1234 seed@localhost:.keys/ssh/passwordLess_serverKey.pem .keys/ssh/passwordLess_serverKey.pem
+echo "*** Copying SSH/GPG keys"
+mkdir -p .keys2/{ssh,gpg,ssl}
+sshpass -p ${LOCAL_PASSWORD} scp -P1234 $LOCAL_USERNAME@localhost:.keys/ssh/passwordLess_serverKey.pem .keys/ssh/passwordLess_serverKey.pem
+sshpass -p ${LOCAL_PASSWORD} scp -P1234 $LOCAL_USERNAME@localhost:.ssh/config .ssh/config
+sshpass -p ${LOCAL_PASSWORD} scp -r -P1234 $LOCAL_USERNAME@localhost:.keys/pgp/ .keys/pgp
 sed -i 's/s33d_ed25519/passwordLess_serverKey.pem/g' ~/.ssh/config
-scp -r -P1234 seed@localhost:.keys/pgp/ .keys/pgp
-scp -P1234 seed@localhost:.ssh/config .ssh/config
 
+echo "*** Adding GPG keys"
 export GPG_TTY=$(tty)
 gpg --list-secret-keys
-
 cd ~/.keys/pgp
 for name in *; do
   if [ ! -d "$name" ]; then
     if [[ ! "$name" =~ '\.sh$' ]] && [ "$name" != 'README.md' ]; then
-      echo "-----> Importing KEY : $name"
+      echo "***  > Importing KEY : $name"
       gpg --import $name
     fi
   fi
 done
-
 gpg --list-secret-keys
 
 # SETUP DOTFILES
+echo "*** Installing Dotfiles"
 cd dotfiles
-gpg --list-secret-keys --keyid-format LONG
-./install.sh 
-./git_setup.sh
-
-
-# UPDATE DOTFILE REMOTE TO SSH
+./install.sh
+./git_setup.sh -u "${GITHUB_FULL_NAME}" -e "${GITHUB_EMAIL}" -k "${GITHUB_GPG_KEY_ID}"
 git remote set-url origin git@github.com:abstracts33d/dotfiles.git
-
-cd
-# INSTALL ZSH THEME
-export ZSH="/home/$USER/.oh-my-zsh"
-export ZSH_CUSTOM="$ZSH/custom"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-
-# ADD MISSING PLUGINS
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+cd ../
 
 # CLONE CONFIGS
-git clone git@github.com:theabstractconnection/configs.git
+echo "*** Cloning Config"
+git clone git@github.com:theabstractconnection/configs.git 2>&1
 
 # INSTALL AMAZON-LINUX-EXTRA PACKAGES
-which amazon-linux-extras
-
-# DOCKER
+# INSTALL DOCKER
+echo "*** Installing Docker"
 sudo amazon-linux-extras enable docker
-sudo yum clean metadata && sudo yum install docker
+sudo yum clean metadata && sudo yum install docker -y
 sudo usermod -a -G docker ec2-user
 sudo systemctl enable docker
-sudo systemctl start docker # NEED TO LOGOUT
+# sudo systemctl start docker # NEED TO LOGOUT
 
 # INSTALL DOCKER-COMPOSE
 # GRAB LATEST RELEASE FROM HERE https://github.com/docker/compose/releases
+echo "*** Installing Docker Compose"
 sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose 
 docker-compose version
 
-# NGINX
-sudo amazon-linux-extras enable nginx1 -y
+# INSTALL NGINX
+echo "*** Installing Nginx"
+sudo amazon-linux-extras enable nginx1
 sudo yum clean metadata && sudo yum install nginx -y
 
+echo "*** Configuring Nginx"
 sudo mkdir /etc/nginx/certs
-sudo scp -P1234 seed@localhost:.keys/ssl/theabstractconnection_cloudflare.key .keys/ssl/theabstractconnection_cloudflare.key
-sudo scp -P1234 seed@localhost:.keys/ssl/theabstractconnection_cloudflare.pem .keys/ssl/theabstractconnection_cloudflare.pem
+sshpass -p ${LOCAL_PASSWORD}  scp -P1234 $LOCAL_USERNAME@localhost:.keys/ssl/theabstractconnection_cloudflare.key .keys/ssl/theabstractconnection_cloudflare.key
+sshpass -p ${LOCAL_PASSWORD}  scp -P1234 $LOCAL_USERNAME@localhost:.keys/ssl/theabstractconnection_cloudflare.pem .keys/ssl/theabstractconnection_cloudflare.pem
 sudo ln -s /home/ec2-user/.keys/ssl/theabstractconnection_cloudflare.key /etc/nginx/certs/theabstractconnection_cloudflare.key
 sudo ln -s /home/ec2-user/.keys/ssl/theabstractconnection_cloudflare.pem /etc/nginx/certs/theabstractconnection_cloudflare.pem
 sudo ln -s /home/ec2-user/configs/the-abstract-connection_nginx_server.conf /etc/nginx/conf.d/server.conf
@@ -89,32 +114,48 @@ sudo systemctl enable nginx
 sudo systemctl start nginx
 
 # INSTALL NVM NODE YARN PM2
-# UNSET NVM_DIR FOR INSTALL
-export NVM_DIR=""
+echo "*** Installing NVM"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
+echo "*** Installing NODE"
 nvm install node
 node -e "console.log('Running Node.js ' + process.version)"
+echo "*** Installing YARN"
 npm install -g yarn
 export PATH="$(yarn global bin):$PATH"
+echo 'export PATH="$(yarn global bin):$PATH"' >> ~/.bashrc
+echo "*** Installing PM2"
 yarn global add pm2
-sudo mkdir /opt/pm2
-sudo ln -s /home/ec2-user/.config/yarn/global/node_modules/.bin/pm2 /opt/pm2/pm2
+sudo mkdir /opt/bin
+sudo ln -s /home/ec2-user/.config/yarn/global/node_modules/.bin/pm2 /opt/bin/pm2
+
+# INSTALL RBENV RUBY BUNDLER
+echo "*** Installing RBENV"
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv 2>&1
+export PATH="$HOME/.rbenv/bin:$PATH"
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+${HOME}/.rbenv/bin/rbenv init
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build 2>&1
+echo "*** Installing RUBY"
+rbenv install 2.6.5
+echo "*** Installing BUNDLER"
+gem install bundler
 
 # INSTALL CODEDEPLOY AGENT
+# NEED RUBY & WGET
 # GET BUCKET_NAME AND REGION_IDENTIFIER FROM HERE
 # https://docs.aws.amazon.com/codedeploy/latest/userguide/resource-kit.html#resource-kit-bucket-names
-export BUCKET_NAME=aws-codedeploy-eu-west-3	
-export REGION_IDENTIFIER=eu-west-3
-
-sudo yum install ruby
-sudo yum install wget
-cd /home/ec2-user
+echo "*** Installing CODEDEPLOY agent"
+export CODEDEPLOY_BUCKET_NAME=aws-codedeploy-eu-west-3	
+echo 'export CODEDEPLOY_BUCKET_NAME=aws-codedeploy-eu-west-3' >> ~/.bashrc
+export AWS_REGION_IDENTIFIER=eu-west-3
+echo 'export AWS_REGION_IDENTIFIER=eu-west-3' >> ~/.bashrc
+cd ${HOME}
 wget -O codeDeployInstall "https://${BUCKET_NAME}.s3.${REGION_IDENTIFIER}.amazonaws.com/latest/install"
 chmod +x ./codeDeployInstall
 sudo ./codeDeployInstall auto
-
 sudo service codedeploy-agent status
 
+echo "*** Creating PROJECT directory"
 # CREATE PROJECTS FOLDER
 mkdir projects && cd projects
